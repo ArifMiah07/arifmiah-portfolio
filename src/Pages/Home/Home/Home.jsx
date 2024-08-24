9bcf89628fb6715af2a7e9d52efe2c7c7e2a7503
ArifMiah07 <arifmiah.me322@gmail.com>
2024-08-24 21:39:03 +0600

    add cover

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import cover from '../../../assets/images/cover.png'
+
+const Home = () => {
+    return (
+        <div className="mx-auto w-[1152px] border border-red-600 ">
+            <div className=''>
+                <img src={cover} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Home;
